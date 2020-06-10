3ffd0ed39fbe56b7e9296d8f0a2a264a70381e2a
Harshit Garg <36312136+harshitgarg01@users.noreply.github.com>
2020-06-10 08:40:45 +0530

    Add files via upload

---
+++ Model2/TimeStorage.h
@@ -0,0 +1,13 @@
+#pragma once
+class TimeStorage
+{
+public:
+	int attempts;
+	chrono::time_point<struct std::chrono::steady_clock, class std::chrono::duration<__int64, struct std::ratio<1, 1000000000> > > identifierCompletionTime;
+	vector <chrono::time_point<struct std::chrono::steady_clock, class std::chrono::duration<__int64, struct std::ratio<1, 1000000000> > >> attemptsStartingTime;
+	vector <chrono::time_point<struct std::chrono::steady_clock, class std::chrono::duration<__int64, struct std::ratio<1, 1000000000> > >> attemptsEndingTime;
+	TimeStorage()
+	{
+		this->attempts = 0;
+	}
+};
