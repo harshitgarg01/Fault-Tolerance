5ea5093040f661a017c8ef434c97cda50d78c061
Harshit Garg <36312136+harshitgarg01@users.noreply.github.com>
2020-06-10 08:42:23 +0530

    Add files via upload

---
+++ Model3/Method1.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <cmath>
+#include <chrono>
+#include <thread>
+#include "MethodParameters.h"
+
+using namespace std;
+
+
+
+class Method1
+{
+public:
+	bool modeGuesser(string identifier, MethodParameters* instance)
+	{
+		this_thread::sleep_for(chrono::milliseconds(20 + rand() % 20));
+		int check = instance->getMode(0);
+		if (check == 0)
+		{
+			int chooser = rand() % 2;
+			if (chooser == 0)
+			{
+				return this->normalMode(identifier, instance);
+			}
+			else
+			{
+				return this->degradedMode(identifier);
+			}
+		}
+		else if (check == 1)
+		{
+			return this->normalMode(identifier, instance);
+		}
+		else
+		{
+			return this->degradedMode(identifier);
+		}
+
+	}
+private:
+	bool normalMode(string identifier, MethodParameters* instance)
+	{
+		int chooser = rand() % 2;
+		if (chooser == 0)
+		{
+			return false;
+		}
+		else
+		{
+			int value = instance->getRetryLimit(0);
+			if (value >= 2) // to restrict method from giving more than 2 correct values per second
+			{
+				return false;
+			}
+			else
+			{
+				instance->setRetryLimit(0, value + 1);
+				return true;
+			}
+
+		}
+	}
+	bool degradedMode(string identifier)
+	{
+		return false;
+	}
+};
